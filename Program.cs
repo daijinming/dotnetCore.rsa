bd37c2fb0038b0e964d61b45620e46fb6b48e073
daijinming <daijinming@live.cn>
2018-12-18 12:03:06 +0800

    初步完成

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace dotnetCore.rsa
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string privateKey = System.IO.File.ReadAllText("private.key").Replace("-----BEGIN RSA PRIVATE KEY-----","").Replace("-----END RSA PRIVATE KEY-----","");
+            string publicKey = System.IO.File.ReadAllText("public.key").Replace("-----BEGIN PUBLIC KEY-----","").Replace("-----END PUBLIC KEY-----","");
+
+
+            var rsa = new RSAHelper(RSAType.RSA2,Encoding.UTF8, privateKey, publicKey);
+
+            string str = "欢迎访问：notepad2.cn";
+
+            Console.WriteLine("原始字符串：" + str);
+
+            //----------------------------------------------加密解密
+
+            /*公钥加密*/
+	        string enStr = rsa.Encrypt(str);
+            
+            Console.WriteLine("加密字符串："+enStr);
+
+            /*私钥解密 */
+	        string deStr = rsa.Decrypt(enStr);
+            Console.WriteLine("解密字符串："+deStr);
+
+
+            //----------------------------------------------签名校验
+
+            /*私钥签名 */
+	        string signStr = rsa.Sign(str);
+            Console.WriteLine("字符串签名：" + signStr);
+
+            /*公钥验证签名 */
+	        bool signVerify = rsa.Verify(str,signStr);
+
+            Console.WriteLine("验证签名：" + signVerify);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # dotnetCore.rsa
-采用openssl生成RSA加密解密算法需要的私钥、公钥。采用.net core 2.0 控制台应用实现RSA加密、解密字符串。
+采用openssl生成RSA算法需要的私钥、公钥。采用.net core 2.1 控制台应用实现基于RSA算法的加密解密和签名校验。
